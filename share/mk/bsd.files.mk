2dbb62d74c96f26d64494e900d3294d53ca0c7d8
Brad Davis <brd@FreeBSD.org>
2018-05-20 04:28:38 +0000

    lets try this again

--- share/mk/bsd.files.mk
+++ share/mk/bsd.files.mk
@@ -85,7 +85,7 @@ _${group}FILES+= ${file}
 
 installdirs-${group}:
 .for dir in ${${group}DIR}
-.if !defined(${_DIR_${dir}}) && empty(${_DIR_${dir}}) && ${_DIR_${dir}} != "true"
+.if !defined(_DIR_${dir}) && empty(_DIR_${dir})
 	@echo installing dirs ${group}DIR ${dir}
 .if defined(NO_ROOT)
 	${INSTALL} ${${group}TAG_ARGS} -d ${DESTDIR}${dir}
