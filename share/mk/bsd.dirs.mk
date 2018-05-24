482d80ce345490fced74168d944fce781bd19ac4
Brad Davis <brd@FreeBSD.org>
2018-05-24 04:20:46 +0000

    Add indentation to make the flow easier to follow

--- share/mk/bsd.dirs.mk
+++ share/mk/bsd.dirs.mk
@@ -9,22 +9,22 @@ __<bsd.dirs.mk>__:
 # <DIR>_OWN, and <DIR>_GRP may be specified.
 DIRS?=
 
-.for dir in ${DIRS:O:u}
-.if defined(${dir}) && !empty(${dir})
+.  for dir in ${DIRS:O:u}
+.    if defined(${dir}) && !empty(${dir})
 # Set default permissions for a directory
 ${dir}_MODE?=	0755
 ${dir}_OWN?=	root
 ${dir}_GRP?=	wheel
-.if defined(${dir}_FLAGS) && !empty(${dir}_FLAGS)
+.      if defined(${dir}_FLAGS) && !empty(${dir}_FLAGS)
 ${dir}_FLAG=	-f ${${dir}_FLAGS}
-.endif
+.      endif
 
-.if defined(NO_ROOT)
-.if !defined(${dir}TAGS) || ! ${${dir}TAGS:Mpackage=*}
+.      if defined(NO_ROOT)
+.        if !defined(${dir}TAGS) || ! ${${dir}TAGS:Mpackage=*}
 ${dir}TAGS+=		package=${${dir}PACKAGE:Uruntime}
-.endif
+.        endif
 ${dir}TAG_ARGS=	-T ${${dir}TAGS:[*]:S/ /,/g}
-.endif
+.      endif
 
 installdirs: installdirs-${dir}
 
@@ -34,11 +34,11 @@ ${DESTDIR}${${dir}}:
 	@echo installing DIRS ${dir}
 	${INSTALL} ${${dir}TAG_ARGS} -d -m ${${dir}_MODE} -o ${${dir}_OWN} \
 		-g ${${dir}_GRP} ${${dir}_FLAG} ${DESTDIR}${${dir}}
-.endif
-.endfor
+.    endif
+.  endfor
 
-.if defined(dir)
+.  if defined(dir)
 realinstall: installdirs
-.endif
+.  endif
 
 .endif
