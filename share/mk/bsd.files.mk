75855611ab95d49f485a0f4db76d32733e762447
Brad Davis <brd@FreeBSD.org>
2018-05-25 23:03:44 +0000

    Eliminate multiple installfiles targets by setting defaults appropriately.
    This also fixes directory creation in the case where each file goes into
    a different directory, like in share/colldef.  Additional testing was
    done with usr.sbin/ntp as the two of these cover some complex cases.
    
    This commit includes lots of debugging .info lines to aid in further
    debugging in case of missed issues.

--- share/mk/bsd.files.mk
+++ share/mk/bsd.files.mk
@@ -32,7 +32,6 @@ ${group}GRP=	${SHAREGRP}
 .endif
 ${group}MODE?=	${SHAREMODE}
 ${group}DIR?=	${BINDIR}
-${group}DIRS?=	${${group}DIR}
 STAGE_SETS+=	${group:C,[/*],_,g}
 STAGE_DIR.${group:C,[/*],_,g}= ${STAGE_OBJTOP}${${group}DIR}
 
@@ -44,70 +43,59 @@ ${group}TAG_ARGS=	-T ${${group}TAGS:[*]:S/ /,/g}
 .endif
 
 
-_${group}FILES=
+.if ${${group}DIR:S/^\///} == ${${group}DIR}
+.info brd: ${${group}DIR:S/^\///} == ${${group}DIR}
+.info and: ${${group}DIR}
+DIRS+=	${${group}DIR}
+_${group}DIR=	${${group}DIR}
+.else
+.info brd: ${${group}DIR:S/^\///} != ${${group}DIR}
+.info and: ${${group}DIR}
+DIRS+=	${group}DIR
+_${group}DIR=	${group}DIR
+.endif
+
+
 .for file in ${${group}}
-.if defined(${group}OWN_${file:T}) || defined(${group}GRP_${file:T}) || \
-    defined(${group}MODE_${file:T}) || defined(${group}DIR_${file:T}) || \
-    defined(${group}NAME_${file:T}) || defined(${group}NAME)
 ${group}OWN_${file:T}?=	${${group}OWN}
 ${group}GRP_${file:T}?=	${${group}GRP}
 .if ${MK_INSTALL_AS_USER} == "yes"
 ${group}OWN_${file:T}=	${SHAREOWN}
 ${group}GRP_${file:T}=	${SHAREGRP}
-.endif
+.endif # ${MK_INSTALL_AS_USER} == "yes"
 ${group}MODE_${file:T}?=	${${group}MODE}
+
+.info ${file} -- ${group}
+.info brd: ${group}DIR_${file}
 .if defined(${group}DIR_${file})
-${group}DIRS+=	${${group}DIR_${file}}
+.info brd yes -- currently: ${${group}DIR}
+${group}DIR+= ${${group}DIR_${file}}
 .else
+.info brd no -- setting to ${${group}DIR}
 ${group}DIR_${file:T}?=	${${group}DIR}
-.endif
+.endif # defined(${group}DIR_${file})
+
 .if defined(${group}NAME)
 ${group}NAME_${file:T}?=	${${group}NAME}
 .else
 ${group}NAME_${file:T}?=	${file:T}
-.endif
+.endif # defined(${group}NAME)
 STAGE_AS_SETS+=	${file:T}
 STAGE_AS_${file:T}= ${${group}NAME_${file:T}}
 # XXX {group}OWN,GRP,MODE
 STAGE_DIR.${file:T}= ${STAGE_OBJTOP}${${group}DIR_${file:T}}
 stage_as.${file:T}: ${file}
 
-installfiles-${group}: installdirs-${group}DIR _${group}INS_${file:T}
+installfiles-${group}: installdirs-${_${group}DIR} _${group}INS_${file:T}
 _${group}INS_${file:T}: ${file}
 	${INSTALL} ${${group}TAG_ARGS} -o ${${group}OWN_${.ALLSRC:T}} \
 	    -g ${${group}GRP_${.ALLSRC:T}} -m ${${group}MODE_${.ALLSRC:T}} \
 	    ${.ALLSRC} \
 	    ${DESTDIR}${${group}DIR_${.ALLSRC:T}}/${${group}NAME_${.ALLSRC:T}}
-.else
-_${group}FILES+= ${file}
-.endif
-.endfor
-
-.if ${${group}DIR:S/^\///} == ${${group}DIR}
-DIRS+=	${${group}DIR}
-_${group}DIR=	${${${group}DIR}}
-.else
-DIRS+=	${group}DIR
-_${group}DIR=	${${group}DIR}
-.endif
-
-.if !empty(_${group}FILES)
-stage_files.${group}: ${_${group}FILES}
-
-installfiles-${group}: installdirs-${group}DIR _${group}INS
-_${group}INS: ${_${group}FILES}
-.if defined(${group}NAME)
-	${INSTALL} ${${group}TAG_ARGS} -o ${${group}OWN} -g ${${group}GRP} \
-	    -m ${${group}MODE} ${.ALLSRC} \
-	    ${DESTDIR}${_${group}DIR}/${${group}NAME}
-.else
-	${INSTALL} ${${group}TAG_ARGS} -o ${${group}OWN} -g ${${group}GRP} \
-	    -m ${${group}MODE} ${.ALLSRC} ${DESTDIR}${_${group}DIR}/
-.endif
-.endif
+.endfor # file in ${${group}}
 
 .endif # defined(${group}) && !empty(${group})
-.endfor
+.endfor # .for group in ${FILESGROUPS}
 
 realinstall: installfiles
 .ORDER: beforeinstall installfiles
